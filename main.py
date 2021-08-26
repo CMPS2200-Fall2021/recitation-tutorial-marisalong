95e3b89d2beff8b6569bfddedae746897ffea66b
Marisa Madelyn Long <marisamlong@Marisas-MacBook-Air.local>
2021-08-26 10:06:58 -0500

    Implemented sum_of_squares and added test 2

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 def sum_of_squares(a):
-	pass
+	sum = 0
+	for i in a:
+		sum += i*i
+	return sum
 
 def test_one():
     assert sum_of_squares([1,2,3]) == 14
+
+def test_two():
+	assert sum_of_squares([3,4,5]) == 50
+
+test_one()
